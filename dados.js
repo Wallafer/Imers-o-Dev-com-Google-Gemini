5eb0a4de363ef361466f3f11392a0a9852068c54
Wallafer <lourival.rbi@gmail.com>
2024-09-05 04:01:17 -0300

    Imersão Dev com Google Gemini

---
+++ dados.js
@@ -0,0 +1,18 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal",
+        descricao : "Rayssa Leal, conhecida como a Fadinha do Skate, é uma jovem skatista brasileira que se tornou um fenômeno mundial. Com apenas 13 anos, Rayssa conquistou uma medalha de prata nos Jogos Olímpicos de Tóquio 2020, tornando-se a atleta mais jovem do Brasil a subir ao pódio olímpico. Sua habilidade e carisma no skate cativaram fãs ao redor do mundo, e ela continua a inspirar jovens atletas com seu estilo único e determinação.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {
+        titulo : "Rebeca Andrade",
+        descricao : "Rebeca Andrade é, sem dúvida, um dos maiores nomes da ginástica artística brasileira. Com um talento inegável e uma determinação ímpar, ela conquistou o coração dos brasileiros e se tornou um símbolo de superação e força. Bicampeã olímpica, Rebeca acumula diversas medalhas em competições nacionais e internacionais, consolidando seu lugar como uma das principais atletas do mundo. Sua trajetória inspiradora, marcada por superação de lesões e desafios, a transformou em um verdadeiro ícone do esporte brasileiro, motivando novas gerações de atletas.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    },
+    {
+        titulo : "Beatriz Souza",
+        descricao : "Beatriz Souza é uma judoca brasileira de destaque internacional na categoria peso-pesado (+78kg). Conhecida por sua força e técnica, ela já conquistou medalhas importantes em campeonatos mundiais e é uma das principais esperanças do Brasil para os próximos Jogos Olímpicos. Beatriz é admirada por sua dedicação ao esporte e sua determinação em alcançar o topo do judô mundial, inspirando muitos jovens atletas a seguirem seus passos.",
+        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    }
+]
+

68fcc2cea2e398b3a8f009dfe1c24ec609967621
Wallafer <lourival.rbi@gmail.com>
2024-09-07 09:20:48 -0300

    Aula 5: imersao alura + google gemini

--- app.js
+++ app.js
@@ -3,18 +3,40 @@ function pesquisar() {
 
     let section = document.getElementById("resultados-pesquisa");
 
+    let campoPesquisa = document.getElementById ("campo-pesquisa").value;
+   
+    if (!campoPesquisa){
+        section.innerHTML = "Nao foi possivel realizar busca."
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase();
     // Inicializa uma string vazia para armazenar os resultados formatados em HTML
     let resultados = "";
-    
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
     // Itera sobre cada dado da pesquisa e cria uma div para cada resultado
     for (let dado of dados) {
-        resultados += `
-        <div class="item-resultado">
-          <h2><a href="#" target="_blank">${dado.titulo}</a></h2>
-          <p class="descricao-meta">${dado.descricao}</p>
-          <a href="${dado.link}" target="_blank">Mais Informações</a>
-        </div>
-      `;
+        titulo = dado.titulo.toLowerCase();
+        descricao = dado.descricao.toLowerCase();
+        tags = dado.tags.toLowerCase();
+        if (titulo.includes(campoPesquisa)||descricao.includes(campoPesquisa)||tags.includes(campoPesquisa)){
+          resultados += `
+          <div class="item-resultado">
+            <h2><a href="#" target="_blank">${dado.titulo}</a></h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href="${dado.link}" target="_blank">Mais Informações</a>
+            <a href="${dado.tags}" target="_blank"></a>
+          </div>
+        `;
+       
+        }
+    }
+
+    if (!resultados){
+        resultados = "Nao foi possivel realizar busca. Nao encontrado na nossa base de dados."
     }
 
     // Atualiza o conteúdo HTML da seção com os resultados gerados
--- dados.js
+++ dados.js
@@ -2,17 +2,19 @@ let dados = [
     {
         titulo : "Rayssa Leal",
         descricao : "Rayssa Leal, conhecida como a Fadinha do Skate, é uma jovem skatista brasileira que se tornou um fenômeno mundial. Com apenas 13 anos, Rayssa conquistou uma medalha de prata nos Jogos Olímpicos de Tóquio 2020, tornando-se a atleta mais jovem do Brasil a subir ao pódio olímpico. Sua habilidade e carisma no skate cativaram fãs ao redor do mundo, e ela continua a inspirar jovens atletas com seu estilo único e determinação.",
-        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags : "Skate, Skateboard, Skatista, Skateboarding, Street Skate, Olímpica, Medalhista Olímpica, Rayssa Leal, Fadinha do Skate, Esportes Radicais, Campeonato Mundial de Skate"
     },
     {
         titulo : "Rebeca Andrade",
         descricao : "Rebeca Andrade é, sem dúvida, um dos maiores nomes da ginástica artística brasileira. Com um talento inegável e uma determinação ímpar, ela conquistou o coração dos brasileiros e se tornou um símbolo de superação e força. Bicampeã olímpica, Rebeca acumula diversas medalhas em competições nacionais e internacionais, consolidando seu lugar como uma das principais atletas do mundo. Sua trajetória inspiradora, marcada por superação de lesões e desafios, a transformou em um verdadeiro ícone do esporte brasileiro, motivando novas gerações de atletas.",
-        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags : "Ginástica, Ginasta, Ginástica Artística, Olímpica, Medalhista Olímpica, Rebeca Andrade, Campeonato Mundial de Ginástica, Solo, Salto, Barra Fixa, Esporte Feminino"
     },
     {
         titulo : "Beatriz Souza",
         descricao : "Beatriz Souza é uma judoca brasileira de destaque internacional na categoria peso-pesado (+78kg). Conhecida por sua força e técnica, ela já conquistou medalhas importantes em campeonatos mundiais e é uma das principais esperanças do Brasil para os próximos Jogos Olímpicos. Beatriz é admirada por sua dedicação ao esporte e sua determinação em alcançar o topo do judô mundial, inspirando muitos jovens atletas a seguirem seus passos.",
-        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags : "Judô, Judoca, Olímpica, Medalhista, Beatriz Souza, Categoria Peso Pesado, Ippon, Esporte de Combate, Artes Marciais, Campeonato Mundial de Judô"
     }
 ]
-
